b049ac519151857b92f04776feec44fd38103c2e
MathewBaldwin <moretwomusic@gmail.com>
2017-05-11 14:16:35 -0400

    Added Db table

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,13 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MemnbershipTypeId { get; set; }
     }
 }
